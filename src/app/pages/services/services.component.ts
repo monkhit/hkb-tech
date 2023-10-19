b0fe9e88fcb36f54448b34c19f040016645f8005
Bharathswamygowda <bharathsgowda111@gmail.com>
2023-10-19 21:25:22 +0300

    update

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -9,8 +9,20 @@ export class ServicesComponent implements OnInit {
 
   videoSource = './assets/video/intro.mp4'
 
+  tabs = [
+    { title: 'Tab 1', content: 'Content for Tab 1' },
+    { title: 'Tab 2', content: 'Content for Tab 2' },
+    { title: 'Tab 3', content: 'Content for Tab 3' }
+  ];
 
   constructor() { }
+
+  activeTab = 0;
+
+  activateTab(index: number): void {
+    console.log(index)
+    this.activeTab = index;
+  }
 
   ngOnInit(): void {
   }
