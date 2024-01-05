4a9d0880bb29ca199fb07796f26c7af3b8b1f118
Bharathswamygowda <bharathsgowda111@gmail.com>
2024-01-05 20:27:20 +0300

    email update

--- src/app/service/email-service.ts
+++ src/app/service/email-service.ts
@@ -8,7 +8,7 @@ export class EmailService {
   constructor(private http: HttpClient) {}
 
   sendEmail(emailData: any) {
-    const emailUrl = 'https://kxjmi5reu5ekyhablkpmj76yvu0dsliq.lambda-url.us-west-2.on.aws/'; // Replace with your server URL
+    const emailUrl = ''; // Replace with your server URL
 
     // Customize your headers as needed
     const headers = new HttpHeaders({
